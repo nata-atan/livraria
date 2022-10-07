7e8ad516b2a795f0f892b3bc8296c50adb2e4681
nata-atan <natanaelmiguelseubio360@gmail.com>
2022-10-07 15:13:56 -0300

    aula 16

---
+++ core/serializers/livro.py
@@ -0,0 +1,13 @@
+from rest_framework.serializers import ModelSerializer
+from core.models import Livro
+
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+
+class LivroDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1
