aa455a75bca3f3415dd635d46f990c862c9f5b91
nata-atan <natanaelmigueleusebio360@gmail.com>
2022-11-22 14:39:16 -0300

    aula 25

--- livraria/settings.py
+++ livraria/settings.py
@@ -10,12 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$6vsa)+q7(tn9711iyr-qg@qgq)o669ajqtxnx+f=!eji7naan"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -84,13 +80,6 @@ WSGI_APPLICATION = "livraria.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
